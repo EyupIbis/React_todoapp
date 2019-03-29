95ed941c9308f998b6c21d1af30b50aad9e4eb9f
EyupIbis <44265485+EyupIbis@users.noreply.github.com>
2019-03-29 12:01:12 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import ToDoItem from "./ToDoItem"
+import todosData from "./todosData"
+
+function App() {
+    const todoItems = todosData.map(item => <ToDoItem key={item.id} item={item}/>)
+
+    return (
+        <div className="todo-list">
+            {todoItems}
+        </div>
+    )
+}
+
+export default App
---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+{
+    id:1,
+    text: "coding",
+    completed: true
+},
+{
+    id:1,
+    text: "programmeren",
+    completed: false
+},
+{
+    id:1,
+    text: "Webdeveloppen",
+    completed: false
+},
+{
+    id:1,
+    text: "webontwikkelen",
+    completed: true
+},
+{
+    id:1,
+    text: "applicaties maken",
+    completed: true
+}
+]
+
+export default todosData
